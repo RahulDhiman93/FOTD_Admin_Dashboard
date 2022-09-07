f7f419b30c33eb54d5bedd6f6720ab087061f579
Rahul Dhiman <rsdm34@gmail.com>
2022-09-08 01:22:22 +0530

    Bg check

--- pages/index.vue
+++ pages/index.vue
@@ -96,4 +96,8 @@ export default {
   }
 }
 </script>
-<style></style>
+<style>
+.table-striped > tbody > tr:nth-child(2n+1) > td, .table-striped > tbody > tr:nth-child(2n+1) > th {
+   background-color: red;
+}
+</style>

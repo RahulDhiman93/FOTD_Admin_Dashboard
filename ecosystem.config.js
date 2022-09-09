e1f7598bdb04d31c8dd8deccf05154519dbee66a
Rahul Dhiman <rsdm34@gmail.com>
2022-09-09 10:31:55 +0530

    Eco system changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     apps: [
       {
-        name: 'FOTD_Admin',
+        name: 'FOTD_Admin_Oracle',
         exec_mode: 'fork',
-        instances: 'max', // Or a number of instances
+        instances: '1', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
         args: 'start'
       }
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         <div class="col-lg-10">
           <input type="text" placeholder="Search any user" v-model="filter" v-on:input="searchUsers"/>
         </div>
-        <el-table :data="usersTableData" thead-class="green-bg bg-dark text-white">
+        <el-table :data="usersTableData">
           <el-table-column
             min-width="65"
             sortable

cd0f92b74a49ec03250b85258aac0590eeadd916
Rahul Dhiman <rsdm34@gmail.com>
2022-10-21 16:17:40 +0530

    index and users fix

--- pages/users.vue
+++ pages/users.vue
@@ -1,123 +1,125 @@
 <template>
-    <div class="row">
-      <div class="col-lg-13">
-        <card card-body-classes="table-full-width">
-          <h4 slot="header" class="card-title">Users</h4>
-          <div class="col-lg-10">
-            <input type="text" placeholder="Search any user" v-model="filter" v-on:input="searchUsers"/>
+  <div class="row">
+    <div class="col-lg-13">
+      <card card-body-classes="table-full-width">
+        <h4 slot="header" class="card-title">Users</h4>
+        <div class="col-lg-10" style="display: flex; justify-content: space-between;">
+          <input type="text" placeholder="Search any user" v-model="filter" v-on:input="searchUsers" style="width: 100%; margin-right: 20px;"/>
+          <div>
+            <button @click="fetchAllUsers">Reload</button>
           </div>
-          <br>
-          <el-table :data="usersTableData" id="mainTable" class="table-responsive">
-            <el-table-column
-              min-width="65"
-              sortable
-              label="UserId"
-              property="user_id"
-            ></el-table-column>
-            <el-table-column
-              min-width="150"
-              label="Name"
-              property="name"
-            ></el-table-column>
-            <el-table-column
-              min-width="170"
-              label="Email"
-              property="email"
-            ></el-table-column>
-            <el-table-column
-              min-width="60"
-              label="Source"
-              property="signup_from"
-            ></el-table-column>
-            <el-table-column
-              min-width="60"
-              label="Rewards"
-              property="reward_points"
-            ></el-table-column>
-            <el-table-column
-              min-width="80"
-              label="Popularity"
-              property="popularity"
-            ></el-table-column>
-            <el-table-column
-              min-width="100"
-              label="Last Login"
-              property="last_login"
-            ></el-table-column>
-          </el-table>
-        </card>
-      </div>
+        </div>
+        <br>
+        <el-table :data="usersTableData" id="mainTable" class="table-responsive">
+          <el-table-column
+            min-width="65"
+            sortable
+            label="UserId"
+            property="user_id"
+          ></el-table-column>
+          <el-table-column
+            min-width="150"
+            label="Name"
+            property="name"
+          ></el-table-column>
+          <el-table-column
+            min-width="170"
+            label="Email"
+            property="email"
+          ></el-table-column>
+          <el-table-column
+            min-width="60"
+            label="Source"
+            property="signup_from"
+          ></el-table-column>
+          <el-table-column
+            min-width="60"
+            label="Rewards"
+            property="reward_points"
+          ></el-table-column>
+          <el-table-column
+            min-width="80"
+            label="Popularity"
+            property="popularity"
+          ></el-table-column>
+          <el-table-column
+            min-width="100"
+            label="Last Login"
+            property="last_login"
+          ></el-table-column>
+        </el-table>
+      </card>
     </div>
-  </template>
-  
-  <script>
-  import config from '@/config';
-  import { Table, TableColumn } from 'element-ui';
-  
-  export default {
-    name: 'Users',
-    components: {
-      [Table.name]: Table,
-      [TableColumn.name]: TableColumn
-    },
-    async created() {
-      await this.fetchAllUsers();
-    },
-    data () {
-      return {
-        filter : "",
-        usersTableData: [],
-        storedTableData: []
-      };
-    },
-    methods: {
-      fetchAllUsers() {
-        fetch(config.BASE_URL + "getAllUsers")
-          .then(response => response.json())
-          .then(data => {
-            this.usersTableData = data.data;
-            this.storedTableData = data.data;
-          })
-        }
-    },
-    computed: {
-      searchUsers() {
-        if (this.filter == "") {
-          this.usersTableData = this.storedTableData;
-        } else {
-          this.usersTableData = this.storedTableData;
-          this.usersTableData = this.usersTableData.filter(row => {
-            const name = row.name.toLowerCase();
-            const email = row.email.toLowerCase();
-            const searchTerm = this.filter.toLowerCase();
-            return email.includes(searchTerm) || name.includes(searchTerm);
-          });
-        }
+  </div>
+</template>
+
+<script>
+import config from '@/config';
+import { Table, TableColumn } from 'element-ui';
+
+export default {
+  name: 'Users',
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn
+  },
+  async created() {
+    await this.fetchAllUsers();
+  },
+  data () {
+    return {
+      filter : "",
+      usersTableData: [],
+      storedTableData: []
+    };
+  },
+  methods: {
+    fetchAllUsers() {
+      fetch(config.BASE_URL + "getAllUsers")
+        .then(response => response.json())
+        .then(data => {
+          this.usersTableData = data.data;
+          this.storedTableData = data.data;
+        })
+      }
+  },
+  computed: {
+    searchUsers() {
+      if (this.filter == "") {
+        this.usersTableData = this.storedTableData;
+      } else {
+        this.usersTableData = this.storedTableData;
+        this.usersTableData = this.usersTableData.filter(row => {
+          const name = row.name.toLowerCase();
+          const email = row.email.toLowerCase();
+          const searchTerm = this.filter.toLowerCase();
+          return email.includes(searchTerm) || name.includes(searchTerm);
+        });
       }
     }
   }
-  </script>
-  <style>
-  #mainTable table thead tr th:nth-child(1){
-    background: #344675;
-  }
-  #mainTable table thead tr th:nth-child(2){
-    background: #344675;
-  }
-  #mainTable table thead tr th:nth-child(3){
-    background: #344675;
-  }
-  #mainTable table thead tr th:nth-child(4){
-    background: #344675;
-  }
-  #mainTable table thead tr th:nth-child(5){
-    background: #344675;
-  }
-  #mainTable table thead tr th:nth-child(6){
-    background: #344675;
-  }
-  #mainTable table thead tr th:nth-child(7){
-    background: #344675;
-  }
-  </style>
-  
+}
+</script>
+<style>
+#mainTable table thead tr th:nth-child(1){
+  background: #344675;
+}
+#mainTable table thead tr th:nth-child(2){
+  background: #344675;
+}
+#mainTable table thead tr th:nth-child(3){
+  background: #344675;
+}
+#mainTable table thead tr th:nth-child(4){
+  background: #344675;
+}
+#mainTable table thead tr th:nth-child(5){
+  background: #344675;
+}
+#mainTable table thead tr th:nth-child(6){
+  background: #344675;
+}
+#mainTable table thead tr th:nth-child(7){
+  background: #344675;
+}
+</style>
